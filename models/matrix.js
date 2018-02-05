bf132529eefb9d08bf11bb61b822ff1856ee2728
Vitaly Telichko <vitaly.telichko@dataart.com>
2018-02-05 18:01:02 +0200

    basic structure

---
+++ models/matrix.js
@@ -0,0 +1,9 @@
+// function Model() {
+//     this.size = {width: 4, height: 4};
+//     this.grid = [];
+// }
+//
+// var model = new Model();
+// console.log(model);
+
+
---
+++ models/summary.js
@@ -0,0 +1,7 @@
+// function Summary(total, best) {
+//     this.total = total;
+//     this.best = best;
+// }
+//
+// var summary = new Summary(0, 0);
+// console.log(summary);
